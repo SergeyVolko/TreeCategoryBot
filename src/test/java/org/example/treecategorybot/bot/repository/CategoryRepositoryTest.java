d87c60707af16b45ba6135c8a8e862b17a18d963
Sergey Volkov <rusobraz@mail.ru>
2024-12-09 21:11:18 +0300

    3 Тестовое задание. Ecxel. Подключение базы данных h2

--- src/test/java/org/example/treecategorybot/bot/repository/CategoryRepositoryTest.java
+++ src/test/java/org/example/treecategorybot/bot/repository/CategoryRepositoryTest.java
@@ -4,11 +4,9 @@ import org.example.treecategorybot.bot.entities.Category;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
-@ActiveProfiles("nativeTest")
 class CategoryRepositoryTest {
     @Autowired
     private CategoryRepository categoryRepository;
